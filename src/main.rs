e2a4e4248246008d4f56141fb543bb83bcf6c8b8
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2023-04-20 22:40:29 -0600

    Additional improvements to speed by checking for the cardiod and bulb interior, optimizing the number of multiplications in our main loop, and checking for periods to exit early

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,138 @@
 use image;
 use std::path;
-use std::vec;
+use std::sync::Arc;
 use tokio;
 
-use std::sync::{Arc, Mutex};
+const MAX_ITERATIONS: u16 = 255;
 
-// use clap::{Parser, Subcommand};
-const MAX_ITERATIONS: u16 = 65535;
-const COL_1: [u8; 3] = [185, 237, 221];
-const COL_2: [u8; 3] = [135, 203, 185];
-const COL_3: [u8; 3] = [85, 157, 170];
-const COL_4: [u8; 3] = [87, 125, 134];
+fn calculate_iterations(x: f32, y: f32) -> u16 {
+    let mut z = [0.0, 0.0];
+    let mut z_2 = [0.0, 0.0];
+    let mut iterations = 0;
 
-struct PixelInfo {
-    x: u32,
-    y: u32,
-    color: [u8; 3],
+    let mut z_old = z;
+    let mut period = 0;
+    while z_2[0] + z_2[1] < 4.0 && iterations < MAX_ITERATIONS {
+        z[1] = (z[0] + z[0]) * z[1] + y;
+        z[0] = z_2[0] - z_2[1] + x;
+        z_2[0] = z[0] * z[0];
+        z_2[1] = z[1] * z[1];
+        iterations += 1;
+
+        if z == z_old {
+            iterations = MAX_ITERATIONS;
+            break;
+        }
+        period += 1;
+        if period == 20 {
+            z_old = z;
+            period = 0;
+        }
+    }
+
+    iterations
 }
 
-#[tokio::main]
-async fn main() {
-    //retrieve command line flags and values
-    // let commandline_arg_matches = clap::clap_app!(mandelbrots_mage =>
-    //     (about: "A command line tool for creating images and GIFs of the Mandelbrot set.")
-    //     (version: "1.0")
-    //     (@arg RESOlUTION: -r --resolution + takes_value + takes_value "The dimensions of the resulting image. (Width by Height). Default is 1920x1080")
-    //     (@arg CENTER: -c --center + takes_value + takes_value "The point of the Mandelbrot set to center the image on. Default is 0+0i")
-    //     (@arg MANDELBROT_SCALE: -s --scale + takes_value + takes_value "The range of real numbers from the Mandelbrot set to display. Default is -2,2. Note that the specified center must be inside this range.")
-    //     (@arg SAVE_PATH: --path + takes_value "The location to save the image or GIF. Default is the current directory.")
-    //     (@arg SHUFFLE_COLORS: --shuffle "Changes what colors represents which degree of instability.")
-    //     (@arg GIF: -g --gif + takes_value + takes_value + takes_value "Creates a GIF zooming in on the Mandelbrot set. The first argument is the number of frames, and the last two arguments are the ending scale. Default is 100 frames, and -.01, .01")
-    // )
-    // .get_matches();
+fn is_in_cardiod_or_bulb(x: f32, y: f32) -> bool {
+    let x = x - 0.25;
+    let y = y * y;
+    let q = x * x + y;
+    let right = q * (q + x);
+    let left = 0.25 * y;
+    right < left
+}
 
-    let resolution = [1920, 1080];
-    let image_location = [-2.0, 2.0, -2.0, 2.0];
+fn get_coordinates_from_pixel_number(
+    pixel_num: u32,
+    resolution: (u32, u32),
+    image_location: (f32, f32, f32, f32),
+) -> (f32, f32) {
+    let x = pixel_num % resolution.0;
+    let y = pixel_num / resolution.0;
 
-    let start_time = std::time::Instant::now();
+    let x = image_location.0
+        + (x as f32 / resolution.0 as f32) * (image_location.1 - image_location.0) as f32;
+    let y = image_location.2
+        + (y as f32 / resolution.1 as f32) * (image_location.3 - image_location.2) as f32;
 
-    let num_workers = 12;
-    let pixels_per_worker = resolution[0] * resolution[1] / num_workers;
+    (x, y)
+}
+
+fn mandelbrot_worker(
+    plot: Arc<Vec<std::sync::atomic::AtomicU16>>,
+    worker_id: u32,
+    pixels_per_worker: u32,
+    resolution: (u32, u32),
+    image_location: (f32, f32, f32, f32),
+    num_workers: u32,
+) {
+    for local_pixel_num in 0..pixels_per_worker {
+        let pixel_num = (local_pixel_num * num_workers) + worker_id;
+        if pixel_num >= resolution.0 * resolution.1 {
+            break;
+        }
+
+        let (x_coord, y_coord) =
+            get_coordinates_from_pixel_number(pixel_num, resolution, image_location);
+
+        let iterations;
+        if is_in_cardiod_or_bulb(x_coord, y_coord) {
+            iterations = MAX_ITERATIONS;
+        } else {
+            iterations = calculate_iterations(x_coord, y_coord);
+        }
+        plot[pixel_num as usize].store(iterations, std::sync::atomic::Ordering::Relaxed);
+    }
+}
+
+fn create_atomic_plot_buffer(resolution: (u32, u32)) -> Vec<std::sync::atomic::AtomicU16> {
+    let plot: Vec<std::sync::atomic::AtomicU16> = (0..resolution.0 * resolution.1)
+        .map(|_| std::sync::atomic::AtomicU16::new(0))
+        .collect();
+    plot
+}
+
+fn convert_atomic_plot_buffer_to_image_buffer(
+    pixels: Vec<std::sync::atomic::AtomicU16>,
+    resolution: (u32, u32),
+) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let mut image_buffer = image::ImageBuffer::new(resolution.0, resolution.1);
+    for (pixel_num, color) in pixels.iter().enumerate() {
+        let x = pixel_num % resolution.0 as usize;
+        let y = pixel_num / resolution.0 as usize;
+        let pixel = image_buffer.get_pixel_mut(x as u32, y as u32);
+        *pixel = image::Rgb([
+            25,
+            30,
+            ((color.load(std::sync::atomic::Ordering::Relaxed) as f32 / MAX_ITERATIONS as f32)
+                * 255.0) as u8,
+        ]);
+    }
+
+    image_buffer
+}
+
+fn generate_mandelbrot_image(
+    pixels: Vec<std::sync::atomic::AtomicU16>,
+    resolution: (u32, u32),
+    bounds: (f32, f32, f32, f32),
+    num_workers: u32,
+) -> Vec<std::sync::atomic::AtomicU16> {
+    let pixels = Arc::new(pixels);
+    let pixels_per_worker = resolution.0 * resolution.1 / num_workers;
     let mut worker_handles = Vec::new();
-    let results: Arc<Mutex<Vec<PixelInfo>>> = Arc::new(Mutex::new(Vec::new()));
 
     for worker_id in 0..num_workers {
-        // let stack = Arc::clone(&stack);
-        let results = Arc::clone(&results);
+        let pixels = Arc::clone(&pixels);
         let handle = std::thread::spawn(move || {
-            let mut local_results = Vec::new();
-
-            for local_pixel_num in 0..pixels_per_worker {
-                let pixel_num = (local_pixel_num * num_workers) + worker_id;
-                if pixel_num >= resolution[0] * resolution[1] {
-                    break;
-                }
-
-                let x = pixel_num % resolution[0];
-                let y = pixel_num / resolution[0];
-
-                let x_coord = image_location[0]
-                    + (x as f64 / resolution[0] as f64)
-                        * (image_location[1] - image_location[0]) as f64;
-                let y_coord = image_location[2]
-                    + (y as f64 / resolution[1] as f64)
-                        * (image_location[3] - image_location[2]) as f64;
-
-                let mut z = [0.0, 0.0];
-                let c = [x_coord, y_coord];
-                let mut iterations = 0;
-
-                while z[0] * z[0] + z[1] * z[1] < 4.0 && iterations < 255 {
-                    let temp = z[0] * z[0] - z[1] * z[1] + c[0];
-                    z[1] = 2.0 * z[0] * z[1] + c[1];
-                    z[0] = temp;
-                    iterations += 1;
-                }
-
-                let color = match iterations {
-                    1..=75 => COL_1,
-                    76..=150 => COL_2,
-                    151..=225 => COL_3,
-                    _ => COL_4,
-                };
-
-                let pixel_info = PixelInfo { x, y, color };
-
-                local_results.push(pixel_info);
-            }
-
-            results.lock().unwrap().extend(local_results);
+            mandelbrot_worker(
+                pixels,
+                worker_id,
+                pixels_per_worker,
+                resolution,
+                bounds,
+                num_workers,
+            )
         });
         worker_handles.push(handle);
     }
@@ -97,22 +141,41 @@ async fn main() {
         handle.join().unwrap();
     }
 
-    // save image
-    let mut image_buffer = image::ImageBuffer::new(resolution[0], resolution[1]);
-    for pixel_info in results.lock().unwrap().iter() {
-        image_buffer.put_pixel(pixel_info.x, pixel_info.y, image::Rgb(pixel_info.color));
+    Arc::try_unwrap(pixels).unwrap()
+}
+
+#[tokio::main]
+async fn main() {
+    let num_tests = 75;
+    let num_workers = 24;
+    let resolution = (1920, 1920);
+    let image_location = (-2.0, 1.0, -1.5, 1.5);
+
+    let start_time = std::time::Instant::now();
+
+    let mut plot = create_atomic_plot_buffer(resolution);
+    for _ in 0..num_tests {
+        plot = generate_mandelbrot_image(plot, resolution, image_location, num_workers);
     }
+    let image_buffer = convert_atomic_plot_buffer_to_image_buffer(plot, resolution);
 
-    println!("Time taken: {}ms", start_time.elapsed().as_millis());
+    let end_time = std::time::Instant::now();
 
-    // let mut dyn_image = image::DynamicImage::ImageRgb8(image_buffer);
-    // dyn_image = dyn_image.resize(x_res, y_res, image::imageops::FilterType::Nearest);
-    // let dyn_save_result = dyn_image.save("./mandelbrot_img.png");
+    println!(
+        "Total time: {}ms",
+        end_time.duration_since(start_time).as_millis()
+    );
+    println!(
+        "Time per solve: {}ms",
+        end_time.duration_since(start_time).as_millis() as f32 / num_tests as f32
+    );
+
+    // save image
     let save_result = image::save_buffer(
         path::Path::new("./mandelbrot_img.png"),
         &image_buffer,
-        resolution[0],
-        resolution[1],
+        resolution.0,
+        resolution.1,
         image::ColorType::Rgb8,
     );
 
